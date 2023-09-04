a005bd8738673efdd96e8c612594944e044d3577
Fatima Najafi <najafifatima762@gmail.com>
2023-09-04 18:01:18 +0430

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Details from './components/Details';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <div className="App">react capsotone porject</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
