2a4e98744620abf1fb583152e4008a501b837471
Fatima Najafi <najafifatima762@gmail.com>
2023-09-06 15:15:44 +0430

    Update the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import logger from 'redux-logger';
-// import companiesReducer from './companySlice';
-
-// const store = configureStore({
-//   reducer: {
-//     companies: companiesReducer,
-//   },
-//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-// });
-// export default store;
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import companiesReducer from './companySlice';
