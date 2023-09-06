268a9b141a54551de4792554a2e397e41dfd186a
Fatima Najafi <najafifatima762@gmail.com>
2023-09-06 06:34:56 +0430

    Update the homepageItem

--- src/components/homepage/HomepageItem.js
+++ src/components/homepage/HomepageItem.js
@@ -1,21 +1,18 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import './HomepageItem.css';
 import company from '../../images/images (1).jpg';
 
-const HomepageItem = (props) => {
-  const { title } = props;
-  HomepageItem.propTypes = {
-    title: PropTypes.string.isRequired,
-  };
-
-  return (
-    <div className="container" data-testid="homepageitem">
-      <div className="item">
-        <img className="icon" src={company} alt="company" />
-        <h1 className="title">{title}</h1>
-      </div>
+const HomepageItem = ({ title }) => (
+  <div className="container" data-testid="homepageitem">
+    <div className="item">
+      <img className="icon" src={company} alt="company" />
+      <h1 className="title">{title}</h1>
     </div>
-  );
+  </div>
+);
+
+HomepageItem.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default HomepageItem;
